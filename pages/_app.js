f6207f46901d29fd000e21b299100f4ea18c795e
ionrusu114 <63353946+ionrusu114@users.noreply.github.com>
2022-11-26 04:07:31 +0200

    Initial commit I(1)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import {createBrowserSupabaseClient} from '@supabase/auth-helpers-nextjs'
+import {SessionContextProvider} from '@supabase/auth-helpers-react'
+import {useState} from "react";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "../dev";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    const [supabase] = useState(() => createBrowserSupabaseClient())
+
+    return (
+        <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <Component {...pageProps} />
+            </DevSupport>
+        </SessionContextProvider>
+    )
 }
 
 export default MyApp
